ed5b0efc0329bce8bbdd6c498c9465255ec35b6c
IsaacLira42 <isaaclira422@gmail.com>
2026-01-11 22:23:41 -0300

    refactor: Centraliza as dependencias em 'backend/src/container/index.ts'

--- backend/src/routes/Auth.Router.ts
+++ backend/src/routes/Auth.Router.ts
@@ -1,30 +1,10 @@
 import { Router } from "express";
-import { UsuarioRepository } from "../repositories/Usuario.Repository";
-import { UsuarioService } from "../services/Usuario.Service";
-import { UsuarioController } from "../controllers/Usuario.Controller";
-import { AuthService } from "../auth/services/AuthServices";
-import { AuthController } from "../auth/controller/AuthController";
 import { AuthMiddleware } from "../auth/middlewares/AuthMiddleware";
-import { ContaService } from "../services/Conta.Service";
-import { ContaRepository } from "../repositories/Conta.Repository";
-
-const usuarioRepository: UsuarioRepository = new UsuarioRepository();
-
-const usuarioService: UsuarioService = new UsuarioService(usuarioRepository);
-const contaService: ContaService = new ContaService(new ContaRepository());
-const usuarioController: UsuarioController = new UsuarioController(
-  usuarioService,
-  contaService
-);
-const authService: AuthService = new AuthService(usuarioRepository);
-const authController: AuthController = new AuthController(
-  authService,
-  usuarioService
-);
+import { container } from "../container";
 
 const AuthRouter = Router();
+const { authController, usuarioController } = container;
 
-// Rota para registro de novos usuários
 AuthRouter.post("/register", usuarioController.create);
 AuthRouter.post("/login", authController.login);
 AuthRouter.get("/me", AuthMiddleware, authController.me);
--- backend/src/routes/Usuario.Router.ts
+++ backend/src/routes/Usuario.Router.ts
@@ -1,22 +1,9 @@
 import { Router } from "express";
-import { UsuarioRepository } from "../repositories/Usuario.Repository";
-import { UsuarioService } from "../services/Usuario.Service";
-import { UsuarioController } from "../controllers/Usuario.Controller";
 import { AuthMiddleware } from "../auth/middlewares/AuthMiddleware";
-import { ContaRepository } from "../repositories/Conta.Repository";
-import { ContaService } from "../services/Conta.Service";
-
-const contaRepository: ContaRepository = new ContaRepository();
-const contaService: ContaService = new ContaService(contaRepository);
-
-const usuarioRepository: UsuarioRepository = new UsuarioRepository();
-const usuarioService: UsuarioService = new UsuarioService(usuarioRepository);
-const usuarioController: UsuarioController = new UsuarioController(
-  usuarioService,
-  contaService
-);
+import { container } from "../container";
 
 const UsuarioRouter = Router();
+const { usuarioController } = container;
 
 UsuarioRouter.get("/:id", AuthMiddleware, usuarioController.findById);
 UsuarioRouter.put("/:id", AuthMiddleware, usuarioController.update);
